164b4b1ff852e18f694d2c92deb53010f99c0ffc
root <root@DESKTOP-A2V93BA.localdomain>
2025-06-01 06:50:38 +0000

    Лабораторна 2. Створення міграція і заповнення даних в таблицях

---
+++ app/Models/BlogCategory.php
@@ -0,0 +1,9 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogCategory extends Model
+{
+    use HasFactory;
+}
