0a158ef8ccb507103d9da53cde1a974f878cc819
Alina Sadovyk <alina.sadovyk@oa.edu.ua>
2025-06-16 21:16:39 +0000

    Лабораторна 15. Vue, API

---
+++ blog-frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLayout>
+        <Navigation />
+        <NuxtPage />
+    </NuxtLayout>
+</template>
+
+<script setup>
+import Navigation from '~/components/Navigation.vue'
+</script>
+
